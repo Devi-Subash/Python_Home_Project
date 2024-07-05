ebbef84b67f713df78935869805c6df2a5f4e3f0
Devi-Subash <vatsaladevi.subash@gmail.com>
2024-07-05 19:54:05 +0100

    Added demo file

---
+++ Devi_Python_Demo.py
@@ -0,0 +1,35 @@
+this_list = ["apple", "banana", "cherry"]
+my_list = ["A", "B", "C", "D" ]
+my_tuple = ( 1, 2, 3, 4, 5)
+my_set = {"a", "b", "c", "d"}
+
+# print(this_list)
+# print(len(this_list))
+# To add contents to list
+# this_list.insert(2, my_tuple)  # this will insert
+# this_list.append(my_tuple) # this will insert in the last
+# mylist.extend(this_list)
+# this_list[2] = "orange" # It will override
+# newlist = this_list + mylist - concatenation
+# this_list[1] = "watermelon"
+# this_list.remove("apple")
+# del this_list[0]
+# this_list.clear()
+# this_list.pop(0)
+# this_list.pop() # removes the last one from the list
+
+
+print(this_list)
+
+
+
+
+# print(newlist)
+'''
+new_list = [this_list[3]] + [this_list[0]] + [this_list[1]] + [this_list [2]]
+
+print(new_list, "This is new list")
+
+print("*", this_list)'''
+
+
