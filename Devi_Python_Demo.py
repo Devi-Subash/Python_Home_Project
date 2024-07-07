64a08093ad6607e9e713246c6f2bfe299486549c
Devi-Subash <vatsaladevi.subash@gmail.com>
2024-07-07 15:57:50 +0100

    Added demo file

--- Devi_Python_Demo.py
+++ Devi_Python_Demo.py
@@ -124,7 +124,86 @@ my_list2 = [7, 8, 9, 11, 7, 9, 0, 12, 41]
 my_new_list = set(my_list1) - set(my_list2)
 print(list(my_new_list))
 
+# 11. write a python program to find the prime numbers from a given list 
+# **********************************************************************
+my_list= [5, 3, 6, 8, 35, 2, 9, 7, 11, 13, 17]
+i=0
+for i in my_list:
+    if my_list[i] % 2 == 0 and my_list[i] % 3 == 0 and my_list[i] % 5 == 0 and my_list[i] == 7:
+        print(f"The given number {my_list[i]} is a prime number")
+    elif my_list[i] in [2,3,5,7]:
+        print(f"The given number {my_list[i]} is a prime number")
+    else:
+        print(f"The given number {my_list[i]} is not a prime number")
 
-# 11. write a python program to find the prime numbers from a given list. 
+# 12. write a python script to add a key value to a dict
+
+my_dict={"name": "Devi", "age": 39}
+print(my_dict)
+my_dict["place"] = "reading"
+print(my_dict)
+my_dict.update({"country":"UK"})
+print(my_dict)
+
+
+# 13. write a python script to concatenate 3 dictionaries
+my_dict1={"name": "Devi", "age": 39}
+my_dict2={"place": "Reading", "Country": "UK"}
+my_dict3={"Skills": "Data", "Work": "Data Engineer"}
+new_dict = {}
+#new_dict = my_dict1 | my_dict2 | my_dict3
+for i in (my_dict1,my_dict2,my_dict3):
+    new_dict.update(i)
+
+print(new_dict)
+
+# 14. write a python script to generate and print a dictionarty that contains a number between 1 and n in the form of x,x2
+
+new_dict = {}
+num = int(input("Enter the value until which the dictionary needs to be populated: "))
+for i in range(1,num+1):
+    new_dict.update({i:i*i})
+print(new_dict)
+
+# 15. write a python program to sort a given dictionary by key
+
+my_dict = {"red": 234, "blue": 939, "yellow": 567, "pink": 453, "orange": 231}
+sorted_dict = {}
+my_list = list(my_dict)
+sort_list = sorted(my_list)
+print(sort_list)
+for i in sort_list:
+    sorted_dict = {i : my_dict[i]}
+    print(sorted_dict)
+
+#*************************************************************
+# 16. write a program to get the max and min values from a dic
+#*************************************************************
+
+my_dict = {"red": 234, "blue": 939, "yellow": 567, "pink": 453, "orange": 231}
+my_list =[]
+min_dict = {}
+max_dict = {}
+for i in my_dict:
+    my_list.append(my_dict[i])
+my_list = sorted(my_list)
+len = len(my_list)
+#min_dict = {my_dict[]:my_list[0]}
+#max_dict = {my_dict[]:my_list[len-1]}
+
+print(my_list)
+print(min_dict)
+print(max_dict)
 
 '''
+
+# 17. write a python program to create a dictionary from a string and track the count of letters from the string
+
+user_input = str(input("Enter the string to "))
+
+my_list2 = []
+my_list2.extend(user_input)
+for i in my_list2:
+
+
+print(my_list2)
